7375684938306b964d82ca80e8a04448d18d769b
최정필 <wjdvlf1407@gmail.com>
2022-06-17 17:03:10 +0900

    service틀 추가

---
+++ src/main/java/com/javaex/vo/BoardVo.java
@@ -0,0 +1,90 @@
+package com.javaex.vo;
+
+public class BoardVo {
+
+	// 필드
+	private int no;
+	private String title;
+	private String content;
+	private int hit;
+	private String regDate;
+	private int userNo;
+
+	// 생성자
+	public BoardVo() {
+	}
+
+	public BoardVo(String title, String content, int hit, String regDate, int userNo) {
+		this.title = title;
+		this.content = content;
+		this.hit = hit;
+		this.regDate = regDate;
+		this.userNo = userNo;
+	}
+
+	public BoardVo(int no, String title, String content, int hit, String regDate, int userNo) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.hit = hit;
+		this.regDate = regDate;
+		this.userNo = userNo;
+	}
+
+	// 메소드 - gs
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getHit() {
+		return hit;
+	}
+
+	public void setHit(int hit) {
+		this.hit = hit;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	// 메소드 - 일반
+	@Override
+	public String toString() {
+		return "BoardVo [no=" + no + ", title=" + title + ", content=" + content + ", hit=" + hit + ", regDate="
+				+ regDate + ", userNo=" + userNo + "]";
+	}
+
+}
