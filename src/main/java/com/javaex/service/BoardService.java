886f3d70e0bd5a21e550942c0a3b7e32bcdd3833
최정필 <wjdvlf1407@gmail.com>
2022-07-05 15:40:55 +0900

    화살표 유무 추가

--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -19,19 +19,23 @@ public class BoardService {
 	// board 리스트
 	public List<BoardVo> getBoardList(String keyword,int crtPage){
 		
-		//페이지 당 게시글 개수
+		// ================= 1 페이지 당 게시글 개수 =================
 		int listCnt = 10;
 		
+		// 페이지의 총 게시글 개수
 		int totalCnt = boardDao.selectTotalCnt();
+		
 		if (crtPage<1) {
 			crtPage=1;
+			
+		//만약 마지막 페이지보다 큰 수를 입력하면 마지막 페이지로 보내기
 		}else if(crtPage>(totalCnt/listCnt)+1){
 			crtPage = (totalCnt/listCnt) +1;
 		}
 		
 		System.out.println(crtPage);
 		
-		//페이지당 버튼 갯수
+		// ================= 페이지당 버튼 갯수 =================
 		int pageBtnCount = 5;
 		
 		//마지막 버튼 번호
@@ -40,10 +44,27 @@ public class BoardService {
 		//시작 버튼 번호
 		int startPageBtnNo = endPageBtnNo - pageBtnCount+1;
 		
-		
+		//마지막 페이지 번호가 게시글 총 개수 * 페이지당 개수를 넘어가지 않게 해준다.
+		// ex: 총 개수: 127개,  11~15페이지일 때   13페이지까지만 나오게 
 		if (endPageBtnNo > totalCnt/listCnt + 1) {
 			endPageBtnNo = totalCnt/listCnt +1;
 		}
+		
+		// 다음 화살표 유무
+		boolean next = false;
+		
+		if (listCnt*endPageBtnNo < totalCnt) {
+			next = true;
+		}
+		
+		
+		// 이전 화살표 유무
+		boolean prev = false;
+		if (startPageBtnNo != 1) {
+			prev = true;
+		}
+		
+		System.out.println("이전 화살표유무: " + prev + ", 다음 화살표유무: " + next);
 		
 		System.out.println("시작번호: " + startPageBtnNo);
 		System.out.println("현재페이지: " + crtPage);
